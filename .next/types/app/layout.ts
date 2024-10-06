68732a5dd009bdf4f76c971cbb3770034414b0b1
Mike Kaufmann <info@mike-kaufmann.com>
2024-10-06 21:47:58 +0200

    update

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/owner/Documents/GitHub/mike-b-kaufmann/app/layout.tsx
+// File: /Users/owner/Documents/GitHub/mike-kaufmann/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/owner/Documents/GitHub/mike-b-kaufmann/app/page.tsx
+// File: /Users/owner/Documents/GitHub/mike-kaufmann/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
